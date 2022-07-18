9d77088afd92986617697e48ab8fd626e885973e
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-18 18:32:00 +0200

    main page idea

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import './style.css'
 
-type Book={
-author: string, 
-title: string, 
-description:string, 
-cover: string
+type Book = {
+  author: string,
+  title: string,
+  description: string,
+  cover: string
 }
 
-type State={
-books: Book[]
+type State = {
+  books: Book[]
 }
 
-let state:State = {
-  books:[]
+let state: State = {
+  books: []
 }
 
 
@@ -25,7 +25,7 @@ function renderHeader() {
   headerEl.className = 'header'
 
   let leftPaneEl = document.createElement('div')
-  leftPaneEl.className='header__left-pane'
+  leftPaneEl.className = 'header__left-pane'
   let leftNavEl = document.createElement('nav')
   let leftUlEl = document.createElement('ul')
   let menuTab = document.createElement('li')
@@ -39,7 +39,7 @@ function renderHeader() {
 
 
   let rightPaneEl = document.createElement('div')
-  rightPaneEl.className='header__right-pane'
+  rightPaneEl.className = 'header__right-pane'
   let rightNavEl = document.createElement('nav')
   let rightUlEl = document.createElement('ul')
   let searchBarLi = document.createElement('li')
@@ -66,11 +66,36 @@ function renderBookList() {
   let mainEl = document.querySelector('#app')
   if (mainEl === null) return
 
-  let bookDisplayEl = document.createElement('div')
-  bookDisplayEl.className = 'main-page'
-  bookDisplayEl.textContent = 'TEXT HERE'
+  let mainPageEl = document.createElement('div')
+  mainPageEl.className = 'main-page'
 
-  mainEl.append(bookDisplayEl)
+  let mainText = document.createElement('h3')
+  mainText.textContent = 'The books everyone is talking about'
+
+  let displayBooksEl = document.createElement('div')
+  displayBooksEl.className = 'books-display'
+
+  let bookItemEl = document.createElement('div')
+
+  let bookCoverEl = document.createElement('img')
+  bookCoverEl.className = 'book-cover'
+  bookCoverEl.src = 'https://images-na.ssl-images-amazon.com/images/I/91eXDiA8GwL.jpg'
+
+
+  let bookTitleEl = document.createElement("h3")
+  bookTitleEl.textContent = 'She Who Became The Sun'
+
+  let bookAuthorEl = document.createElement("h4")
+  bookAuthorEl.textContent = 'Shelley Parker-Chan'
+
+  let bookPriceEl = document.createElement("h4")
+  bookPriceEl.textContent = '£15'
+
+  bookItemEl.append(bookCoverEl, bookTitleEl, bookAuthorEl, bookPriceEl)
+  displayBooksEl.append(bookItemEl)
+  mainPageEl.append(mainText, displayBooksEl)
+  mainEl.append(mainPageEl)
+
 
 }
 
