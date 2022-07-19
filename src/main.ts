e44eae8ed7966d4c3f363915a28e2663972288b0
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-19 10:50:11 +0200

    state update

--- src/main.ts
+++ src/main.ts
@@ -25,13 +25,8 @@ function getBookdata() {
     })
 }
 
-<<<<<<< HEAD
-// getMoviedata()
-// window.state = state
-=======
 getBookdata()
 window.state = state
->>>>>>> ec3e32683e6a1102445121d35a16da6283e71613
 
 function renderHeader() {
   let mainEl = document.querySelector('#app')
